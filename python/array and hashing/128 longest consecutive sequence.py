fc160d653ab51b2f4d08d438036d151af15fa54b
Joey Wang <joeywangzr@gmail.com>
2023-03-04 17:29:14 -0500

    128

---
+++ python/array and hashing/128 longest consecutive sequence.py
@@ -0,0 +1,13 @@
+nums = [100,4,200,1,3,2]
+
+numset = set(nums)
+longest = 0
+for i in range(len(numset)):
+    # start counting from start of a consecutive sequence
+    counter = 0
+    if ((i-1) not in numset) and (i in numset):
+        while (i+counter) in numset:
+            counter += 1
+    longest = max(longest, counter)
+
+print(longest)
