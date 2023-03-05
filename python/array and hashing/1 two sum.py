e0bcc1f590677c5937c2c03199357b49802fcb2b
Joey Wang <joeywangzr@gmail.com>
2023-03-05 15:43:30 -0500

    fix 2sum

--- python/array and hashing/1 two sum.py
+++ python/array and hashing/1 two sum.py
@@ -1,11 +1,21 @@
 target = 7
 nums = [3,2,4,10,20]
-dict = {}
 
+# solution 1: map of diff to index  
+dict = {}
 for i in range(len(nums)):
-    diff = target - nums[i]
-    
     if nums[i] in dict.keys():
         print([i, dict[nums[i]]])
+        break
 
-    dict[diff] = i
+    dict[target-nums[i]] = i
+
+# solution 2: map of value to index
+dict = {}
+for i, n in enumerate(nums):
+    diff = target - n
+    if diff in dict.keys():
+        print([i, dict[diff]])
+        break
+    
+    dict[n] = i
