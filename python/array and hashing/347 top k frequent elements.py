979f13688e67faf2da2d328fa47509bb4aebeb75
Joey Wang <joeywangzr@gmail.com>
2023-04-29 17:10:24 -0600

    top k elements

--- python/array and hashing/347 top k frequent elements.py
+++ python/array and hashing/347 top k frequent elements.py
@@ -1,19 +1,22 @@
 # use a bucket search approach
-nums = [1, 1, 1, 2, 2, 3]
+nums = [1,1,1,2,2,3]
+# nums = [-1,-1]
 k = 2
 
 hashmap = {}
-solution = [[] for i in range(len(nums))]
 for i in nums:
-    hashmap[i] = hashmap.get(i, 0) + 1
+    hashmap[i] = hashmap.get(i,0) + 1
 
-for i in hashmap.items():
-    solution[i[1]].append(i[0])
+buckets = [[] for _ in range(len(nums)+1)]
 
-answer = []
-for i in range(len(solution)-1, 0, -1):
-    for j in solution[i]:
-        if len(answer) == k:
-            print(answer)
-        answer.append(j)
-print(answer)
+for j in hashmap:
+    index = hashmap[j]
+    buckets[index].append(j)
+
+ans = []
+print(buckets)
+for h in range(len(buckets)-1,-1,-1):
+    for l in buckets[h]:
+        ans.append(l)
+        if len(ans) == k:
+            print(ans)
