1b8844a1fa6479f8c8afe8a649b7f6d6900d5210
Joey Wang <joeywangzr@gmail.com>
2023-04-30 10:22:19 -0600

    valid sudoku

--- python/array and hashing/36 valid sudoku.py
+++ python/array and hashing/36 valid sudoku.py
@@ -1,18 +1,30 @@
 import collections
 
-board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+board = [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
 
-rows = collections.defaultdict(set)
-columns = collections.defaultdict(set)
+row = collections.defaultdict(set)
+column = collections.defaultdict(set)
 box = collections.defaultdict(set)
 
-for r in range(9):
-    for c in range(9):
-        value = board[r][c]
-        if (value != ".") and ((value in rows[r]) or (value in columns[c]) or (value in box[(r // 3, c // 3)])):
+for i in range(9):
+    for j in range(9):
+        value = board[i][j]
+        if value == ".":
+            continue
+        if ((value in row[i]) or (value in column[j]) or (value in box[(i//3,j//3)])):
             print(False)
-        rows[r].add(value)
-        columns[c].add(value)
-        box[(r // 3, c // 3)].add(value)
-print(rows)
+        
+        row[i].add(value)
+        column[j].add(value)
+        box[(i//3,j//3)].add(value)
+
 print(True)
+        
