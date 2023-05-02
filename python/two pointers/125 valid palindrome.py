c1995526f4944ca56450498d05e485af2addd5cd
Joey Wang <joeywangzr@gmail.com>
2023-05-01 23:37:53 -0600

    valid palindrome

--- python/two pointers/125 valid palindrome.py
+++ python/two pointers/125 valid palindrome.py
@@ -3,8 +3,8 @@ s = "race a car"
 
 s = re.sub('[\W_]+', '', s).lower()
 
-for i in range(len(s)-1):
+for i in range(-(len(s)//-2)): # ceil division
     if s[i] != s[len(s)-1-i]:
         print(False)
-        break
+
 print(True)
