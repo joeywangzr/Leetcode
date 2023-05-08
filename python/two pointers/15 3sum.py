6ef9867f6e4f264172492bfc6e64b0bf015b9590
Joey Wang <joeywangzr@gmail.com>
2023-05-08 11:39:55 -0400

    3sum

--- python/two pointers/15 3sum.py
+++ python/two pointers/15 3sum.py
@@ -1,28 +1,30 @@
-# nums = [-1,0,1,2,-1,-4]
-nums = [0,0,0]
-solution = []
+nums = [-1,0,1,2,-1,-4]
+nums = [0,0,0,0]
+nums = [1,-1,-1,0]
+nums = [-2,0,0,2,2]
 
+sol = []
 nums.sort()
-
 for i in range(len(nums)):
     if (nums[i] == nums[i-1]) and (i>0):
         continue
     else:
-        # do two sum        
         l = i+1
         r = len(nums)-1
-        while l<r:
+        while l < r:
             sum = nums[i] + nums[l] + nums[r]
+            
             if sum == 0:
-                solution.append([nums[i], nums[l], nums[r]])
-                l += 1
+                if [nums[i],nums[l],nums[r]] in sol:
+                    continue
+                else:
+                    sol.append([nums[i],nums[l],nums[r]])
                 r -= 1
-                while nums[l] == nums [l-1] and l<r:
+                while nums[l] == nums[l-1] and l < r:
                     l += 1
             elif sum < 0:
                 l += 1
             elif sum > 0:
                 r -= 1
-            
 
-print(solution)
+print(sol)
